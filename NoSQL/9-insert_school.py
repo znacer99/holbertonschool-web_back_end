3154bc4bda0b0794f870507b0190715cc3db4d50
root <znacer99@gmail.com>
2023-09-08 07:41:18 -0700

    9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+inserts a new document in  a collection based on kwargs
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection
+    """
+
+    return mongo_collection.insert_one(kwargs).inserted_id

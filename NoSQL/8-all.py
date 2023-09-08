86811ceebe2ea3ceda96a1c211d0e7cbb5a5746e
root <znacer99@gmail.com>
2023-09-08 07:36:15 -0700

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+List all documents in a collection using python
+"""
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection
+    """
+    return mongo_collection.find()

a4f935f34f76aed55aae67e825ec75b677ef8463
root <znacer99@gmail.com>
2023-09-08 15:23:34 -0700

    0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,7 @@ return a tuple containing the start and end indexes
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple containing the start and end indexes

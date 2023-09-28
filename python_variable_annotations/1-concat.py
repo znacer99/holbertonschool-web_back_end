c34a3dfd8a5fdc29b8bdab39505e169633c80a00
root <znacer99@gmail.com>
2023-09-28 10:32:39 -0700

    concat strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat
+"""
+
+
+def concat(str1 :str, str2: str) -> str:
+    """
+    Concatenate two strings and return a string in result.
+    """
+    return str1 + str2

7bf42898d84c7a97e13796b58e1b4de3b3c5fad4
znacer99 <naceurez95@gmail.com>
2023-10-07 17:25:38 +0000

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a type annotated function
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Sum elements in a list
+    """
+    return sum(input_list)

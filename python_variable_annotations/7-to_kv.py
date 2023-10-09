4e65c02aa9e35b1d7cae0d6950580069ad3d02df
znacer99 <naceurez95@gmail.com>
2023-10-09 22:02:25 +0000

    7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,6 @@ a type-annotated function Tuple
 from typing import Union, Tuple
 
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, v * v)

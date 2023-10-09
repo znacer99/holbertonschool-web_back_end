5415b72c4a842db3670999c9adfb1e999f3bd3d0
znacer99 <naceurez95@gmail.com>
2023-10-09 20:57:42 +0000

    4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""
-set annotated variables
-"""
+"""Defines and annotates the following variables"""
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school : str = "Holberton"
+school: str = "Holberton"

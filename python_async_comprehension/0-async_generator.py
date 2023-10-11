711b66779c423d3f44c2272d002c41b5c5f2939f
znacer99 <naceurez95@gmail.com>
2023-10-11 09:05:50 +0000

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,13 @@ after an asynchronous delay.
 """
 import asyncio
 import random
+from typing import Generator
 
 
 async def async_generator():
+    """
+    asyncio generator bla bla bla
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)

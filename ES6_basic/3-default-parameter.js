31558f5a369f7b5b9dd3e2da34ee5e6feff9db2d
znacer99 <naceurez95@gmail.com>
2023-10-11 23:59:01 +0000

    3

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,5 @@
 const getSumOfHoods = (initialNumber,
-  expansion989 = 89,
+  expansion1989 = 89,
   expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
 
 export default getSumOfHoods;

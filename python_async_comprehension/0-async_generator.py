bbb11aaaee52b5ca88e20a38fd4e005e28d29d46
znacer99 <naceurez95@gmail.com>
2023-10-11 09:36:09 +0000

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     asyncio generator bla bla bla
     """

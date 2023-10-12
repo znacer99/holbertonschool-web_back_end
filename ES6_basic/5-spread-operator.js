ae01e1be1d14c43f95d571cc3509f3ae7a8b1834
znacer99 <naceurez95@gmail.com>
2023-10-12 00:07:49 +0000

    5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-const concatArrays(array1, array2, string) => [].concat(...array1, ...array2, ...string);
+const concatArrays = (array1, array2, string) => [].concat(...array1, ...array2, ...string);
 
 module.exports = concatArrays;
